8923fa17f7e6320cb9a48493a1a1d61600034366
Matt Kalafut <Mkalafut91@gmail.com>
2024-12-05 00:02:04 -0600

    Modify input action to reduce health and display death message when health reaches 0

--- main.gd
+++ main.gd
@@ -4,21 +4,33 @@ var health = 100;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = "Health: %s" % str(health)  # $Label gets the scene node called "Label" to modify it with code.
+	$Label.text = "Health: %s. You are healthy" % str(health)  # $Label gets the scene node called "Label" to modify it with code.
 	$Label.modulate = Color.GREEN	
 	print(health)
 	
 func _input(event):
 	if event.is_action_pressed("my_action"): # my_action is bound to spacebar in Project Settings > Input Map
-		if health > 0:
-			health -= 10
-			$Label.text = "Health: %s" % str(health) # String interpolation with GDScript https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_format_string.html
-			$Label.modulate = Color.RED
-			print(health)
+		health -= 10
+		$Label.modulate = Color.RED
+		print(health)
+		
+		if health <= 0:
+			health = 0
+			$Label.text = "You died!"
+			print("You died!")
+		elif health <= 50:
+			$Label.text = "Health: %s. You are injured" % str(health)
+			print("You are injured")
+		else:
+			$Label.text = "Health: %s. You are healthy" % str(health)
+			print("You are healthy")
+		
 		
 	if event.is_action_released("my_action"):
-		if health > 0:
+		if health > 50:
 			$Label.modulate = Color.GREEN
+		elif health <= 50 and health > 0: # In GDScript, we use and/or instead of &&/||
+			$Label.modulate = Color.YELLOW
 		else:
 			$Label.modulate = Color.RED
-			$Label.text = "YOU DIED"
+		

34953ab86328bcd92d73862b697ddb90482a64d2
Matt Kalafut <Mkalafut91@gmail.com>
2024-12-04 23:18:27 -0600

    Basic variable operations.
    Remove semicolons. Adding them caused indentation to act up in Rider and since this mirrors python, I'll just follow convention

--- main.gd
+++ main.gd
@@ -1,16 +1,20 @@
-
 extends Node
 
+var health = 100;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = "Hello, world"; # $Label gets the scene node called "Label" to modify it with code.
-	$Label.modulate = Color.GREEN;
+	$Label.text = "Hello, world" # $Label gets the scene node called "Label" to modify it with code.
+	$Label.modulate = Color.GREEN
 	
+	health = 40 # Assign to value
+	health = 20 + 30 # Assign to calculation
+	health += 20 # Adds 20 to current health. -=, *= and /= do the same for their operations
+	print(health)
 	
 func _input(event):
 	if event.is_action_pressed("my_action"): # my_action is bound to spacebar in Project Settings > Input Map
-		$Label.modulate = Color.RED;
+		$Label.modulate = Color.RED
 		
 	if event.is_action_released("my_action"):
-		$Label.modulate = Color.GREEN;
+		$Label.modulate = Color.GREEN

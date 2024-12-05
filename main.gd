9adc7763edb6c9f96ac29611340a0bed566d545c
Matt Kalafut <Mkalafut91@gmail.com>
2024-12-04 23:01:55 -0600

    Add label node. Referenced within the script allowing properties to change with code

--- main.gd
+++ main.gd
@@ -3,4 +3,5 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hello, world!");
+	$Label.text = "Hello, world"; # $Label gets the scene node called "Label" to modify it with code.
+	$Label.modulate = Color.GREEN;

2c8603ab4cb3914bd9a6af9d3c00b22ad171e008
Matt Kalafut <Mkalafut91@gmail.com>
2024-12-04 23:43:19 -0600

    Modify input action to reduce health and display death message

--- main.gd
+++ main.gd
@@ -4,17 +4,21 @@ var health = 100;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = "Hello, world" # $Label gets the scene node called "Label" to modify it with code.
-	$Label.modulate = Color.GREEN
-	
-	health = 40 # Assign to value
-	health = 20 + 30 # Assign to calculation
-	health += 20 # Adds 20 to current health. -=, *= and /= do the same for their operations
+	$Label.text = "Health: %s" % str(health)  # $Label gets the scene node called "Label" to modify it with code.
+	$Label.modulate = Color.GREEN	
 	print(health)
 	
 func _input(event):
 	if event.is_action_pressed("my_action"): # my_action is bound to spacebar in Project Settings > Input Map
-		$Label.modulate = Color.RED
+		if health > 0:
+			health -= 10
+			$Label.text = "Health: %s" % str(health) # String interpolation with GDScript https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_format_string.html
+			$Label.modulate = Color.RED
+			print(health)
 		
 	if event.is_action_released("my_action"):
-		$Label.modulate = Color.GREEN
+		if health > 0:
+			$Label.modulate = Color.GREEN
+		else:
+			$Label.modulate = Color.RED
+			$Label.text = "YOU DIED"
